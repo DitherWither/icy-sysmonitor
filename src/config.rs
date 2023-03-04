a3ddaae6232456370bc47e8faa37b260f5fbb681
Vardhan Patil(DitherWither) <demonslayervardhan@gmail.com>
2023-03-04 15:02:05 +0530

    Improve error handling for the config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use directories_next::ProjectDirs;
 use serde::{Deserialize, Serialize};
 
@@ -25,24 +27,64 @@ impl Config {
         let config_path = config_dir.join("config.toml");
 
         // Load the config from disk if it exists
-        // TODO: Add error handling
+        // TODO: Make this display a dialog instead of printing to stderr
         if config_path.exists() {
-            let config = std::fs::read_to_string(config_path).expect("Could not read config file");
+            let config = match std::fs::read_to_string(config_path) {
+                Ok(config) => config,
+                Err(_) => {
+                    eprintln!("Could not read config file, defaulting to default values");
+                    eprintln!("Please check the permissions of the config file");
 
-            toml::from_str(&config).expect("Could not parse config file")
+                    return Self::default();
+                }
+            };
+
+            match toml::from_str(&config) {
+                Ok(config) => config,
+                Err(_) => {
+                    eprintln!("Could not parse config file, defaulting to default values");
+                    eprintln!("Please check the config file for errors");
+
+                    return Self::default();
+                }
+            }
         } else {
             // Create the config directory if it does not exist
             if !config_dir.exists() {
-                std::fs::create_dir_all(config_dir).expect("Could not create config directory");
+                match std::fs::create_dir_all(config_dir){
+                    Ok(_) => {},
+                    Err(_) => {
+                        eprintln!("Could not create config directory");
+                        eprintln!("Please check the permissions of the config directory");
+
+                        return Self::default();
+                    }
+                }
             }
 
             // Create a new config with the default values
             let config = Self::default();
 
             // Write the config to disk
-            let config_str = toml::to_string(&config).expect("Could not serialize config");
+            let config_str = match toml::to_string(&config) {
+                Ok(config) => config,
+                Err(_) => {
+                    eprintln!("Could not serialize the default config");
+                    eprintln!("How did this even happen?");
 
-            std::fs::write(config_path, config_str).expect("Could not write config file");
+                    return config;
+                }
+            };
+
+            match std::fs::write(config_path, config_str) {
+                Ok(_) => {},
+                Err(_) => {
+                    eprintln!("Could not write config file, defaulting to default values");
+                    eprintln!("Please check the permissions of the config directory");
+
+                    return config;
+                }
+            }
 
             config
         }
